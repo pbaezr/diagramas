3ad9b0db92423f3d81ec25eac0a1d429873ec76e
Pablo Báez R <pbaez@ug.uchile.cl>
2019-05-28 16:42:05 -0400

    Se guardan parámetros de materiales con setappdata
    Además se elimina el pushbutton de Matlab y se reemplaza por un JToggleButton y se elimina la función graficarDiagrama (la cual es redefinida en un nuevo archivo)

--- diagramas.m
+++ diagramas.m
@@ -27,6 +27,8 @@ end
 
 function diagramas_OpeningFcn(hObject, ~, handles, varargin)
 
+movegui(handles.figure1,'center')
+
 path = pwd;
 if isdeployed, path = fileparts(mfilename('fullpath')); end
 addpath(fullfile(path,'lib'))
@@ -93,13 +95,14 @@ end
 % definición de parámetros iniciales para las curvas tensión-deformación de los materiales
 % (aplicables solo para el diagrama M-phi, a excepción de f'c y fy)
 parametros.modeloAcero = {1 'Elastoplástico'};
-parametros.ef = 0.2;
 parametros.fy = 420;
+parametros.ef = 0.15;
+parametros.Es2 = 0;
 parametros.modeloHormigon = {1 'Saenz'};
 parametros.fc = 30;
 parametros.e0 = 0.002;
 parametros.modeloHormigonTrac = {1 'sin tracción'};
-handles.parametros = parametros;
+setappdata(handles.figure1,'parametrosMateriales',parametros); %handles.parametros = parametros;
 
 % creación de figura explicativa de los inputs
 axes(handles.axes2)
@@ -123,6 +126,13 @@ annotation(handles.uipanel1,'doublearrow',[0.185 0.185],[0.07 0.25],'head1width'
 annotation(handles.uipanel1,'doublearrow',[0.7 0.7],[0.1 0.25],'head1width',3,'head2width',3,'head1length',3,'head2length',3,'headstyle','vback3')
 annotation(handles.uipanel1,'doublearrow',[0.8 0.8],[0.22 0.25],'head1width',3,'head2width',3,'head1length',3,'head2length',3,'headstyle','vback3')
 annotation(handles.uipanel1,'doublearrow',[0.24 0.64],[0.045 0.045],'head1width',3,'head2width',3,'head1length',3,'head2length',3,'headstyle','vback3')
+
+% crear un botón y asociarlo a la definición de las leyes constitutivas de los materiales
+handles.boton = javacomponent(javax.swing.JToggleButton('Leyes Constitutivas'),[7 155 115 25],handles.uipanel2);
+handles.boton.setFont(java.awt.Font('Segoe ui',0,11));
+handles.boton.setMargin(java.awt.Insets(0,0,0,0));
+handles.boton.setEnabled(false);
+crearInterfazMateriales(handles,path);
 
 % Choose default command line output for diagramas
 handles.output = hObject;
@@ -178,13 +188,17 @@ catch
 end
 
 function input_fc_Callback(hObject, ~, handles)
-fc = [16 20 25 30 35]; % valores de f'c en MPa más usuales (en Chile, por lo menos)
-handles.parametros.fc = fc(hObject.Value); % el objeto es un popupmenu
+fc = [16 20:5:55]; % valores de f'c en MPa
+hObject.UserData = fc(hObject.Value);
+setappdata(handles.figure1,'parametrosMateriales',...
+    setfield(getappdata(handles.figure1,'parametrosMateriales'),'fc',hObject.UserData))
 graficarDiagrama(handles)
 
 function input_fy_Callback(hObject, ~, handles)
 fy = [280 420]; % valores de fy en MPa típicos para el acero utilizado en hormigón armado
-handles.parametros.fy = fy(hObject.Value);
+hObject.UserData = fy(hObject.Value);
+setappdata(handles.figure1,'parametrosMateriales',...
+    setfield(getappdata(handles.figure1,'parametrosMateriales'),'fy',hObject.UserData))
 graficarDiagrama(handles)
 
 function radiobutton1_KeyPressFcn(~, eventdata, handles)
@@ -217,155 +231,17 @@ flag = handles.radiobutton1.Value == 1;
 
 % mostrar, ocultar o inhabilitar objetos según la opción
 flags={'off' 'on'};
+handles.boton.setEnabled(~flag);
 handles.textoError.setEnabled(~flag);
 handles.uipanel3.Visible = flags{flag+1};
 handles.uipanel4.Visible = flags{~flag+1};
-obj = {'pushbutton1' 'input_n' 'input_error' 'input_P' 'text16' 'text17' 'text33' 'text34' 'text48' 'text49'};
+obj = {'input_n' 'input_error' 'input_P' 'text16' 'text17' 'text33' 'text34' 'text48' 'text49'};
 for i = 1:length(obj), handles.(obj{i}).Enable = flags{~flag+1}; end
 
 graficarDiagrama(handles) % graficar el diagrama correspondiente
 
 function uibuttongroup2_SelectionChangedFcn(~, ~, handles)
 input_carga_Callback(handles.input_carga, [], handles)
-
-function pushbutton1_Callback(hObject, ~, handles)
-% definición de los prámetros de las curvas tensión-deformación de los materiales
-parametros2 = materiales(handles.parametros); % si no hay modificación, parametros2 = []
-
-if ~isempty(parametros2)
-    handles.parametros = parametros2; % almacenar los nuevos parámetros      
-    handles.text48.String = ['Acero: ',parametros2.modeloAcero{2}];
-    handles.text49.String = ['Hormig: ',parametros2.modeloHormigon{2},', ',parametros2.modeloHormigonTrac{2}];
-    guidata(hObject,handles);
-    graficarDiagrama(handles) % recalcular el diagrama M - phi
-end
-
-function graficarDiagrama(handles)
-handles.figure1.Pointer = 'watch';
-
-% obtener valores de los inputs
-b = handles.input_b.UserData; % ancho de la sección (en mm)
-h = handles.input_h.UserData; % altura de la sección (en mm)
-d2 = handles.input_d2.UserData; % ubicación de la armadura As2 (en mm)
-d1 = handles.input_d1.UserData; % ubicación de la armadura As1 (en mm)
-As2 = handles.input_As2.UserData; % área total de la armadura As2 (en mm^2)
-As1 = handles.input_As1.UserData; % área total de la armadura As1 (en mm^2)
-
-if ~isempty(b) && ~isempty(h) && ~isempty(d2) && ~isempty(d1) && ~isempty(As2) && ~isempty(As1)
-
-    axes(handles.axes1), cla
-    grid on, hold on
-    set(handles.axes1,'xlimmode','auto','ylimmode','auto')
-    
-    % verificar la congruencia de los valores de d1 y d2
-    if d2 > h || d1 > h || d2 > d1
-        errordlg('Los valores de d1 y d2 no deben ser mayores que h, además de que d1 > d2.','Incongruencia en los datos','modal')
-        handles.figure1.Pointer = 'arrow';
-        handles.datosValidos = false;
-        return
-    end
-    handles.datosValidos = true; % actualizar estatus
-    handles.armadurasSimetricas = As1 == As2 && d2 == h-d1;
-
-    if handles.radiobutton1.Value == 1 % diagrama P - M
-        % obtención, conversión de unidades y almacenamiento de los resultados
-        [M1,P,c1,phi1] = interaccionPM(b,h,d1,d2,As1,As2,handles.parametros.fc,handles.parametros.fy);
-        M1 = 0.1*M1; % conversión de kN-m a tonf-m
-        P = 0.1*P; % conversión de kN a tonf
-        c1 = 0.1*c1; % conversión de mm a cm
-        resultadosPM.M1 = M1;
-        resultadosPM.P = P;
-        resultadosPM.c1 = c1;
-        
-        % identificar el límite superior de compresión por concepto de la excentricidad accidental, que solo aplicaría a columnas y no a muros 
-        [~,ind] = min(abs(P-0.8*P(end)));        
-        
-        % graficar curva Pn - Mn
-        curvaNominal = plot(M1,P,'linewidth',1);            
-
-        % graficar curva reducida por el factor phi
-        curvaReducida = plot([phi1(1:ind).*M1(1:ind) 0],[phi1(1:ind).*P(1:ind) phi1(ind)*P(ind)],'color','r');
-        plot(phi1(ind:end).*M1(ind:end),phi1(ind:end).*P(ind:end),'--','color','r')
-
-        % graficar rama M < 0 si es necesario (sección armada de forma no simétrica)
-        if ~handles.armadurasSimetricas            
-            [M2,~,c2,phi2] = interaccionPM(b,h,d1,d2,As1,As2,handles.parametros.fc,handles.parametros.fy,-1);
-            M2 = 0.1*M2;
-            c2 = 0.1*c2;
-            resultadosPM.M2 = M2;
-            resultadosPM.c2 = c2;            
-            
-            plot(M2(end:-1:1),P(end:-1:1),'linewidth',1,'color',[0 0.447 0.741]);            
-            plot([0 phi2(ind:-1:1).*M2(ind:-1:1)],[phi2(ind)*P(ind) phi2(ind:-1:1).*P(ind:-1:1)],'color','r');
-            plot(phi2(end:-1:ind).*M2(end:-1:ind),phi2(end:-1:ind).*P(end:-1:ind),'--','color','r')                        
-            plot([0 0],ylim,'color',[0.5 0.5 0.5]) % trazar recta para M = 0
-        end
-        
-        plot(xlim,[0 0],'color',[0.5 0.5 0.5]) % trazar recta para P = 0
-
-        leyenda = legend([curvaNominal curvaReducida],{'P_n - M_n','\phiP_n - \phiM_n'});
-        set(leyenda,'fontsize',8,'location','northeast');
-
-        xlabel('M [tonf\timesm]')
-        ylabel('P [tonf]')
-
-        handles.resultadosPM = resultadosPM;
-    else%if handles.radiobutton2.Value == 1 % diagrama M - phi
-        % obtener parámetros para el análisis seccional
-        n = handles.input_n.UserData;
-        tol = handles.input_error.UserData;
-        P = handles.input_P.UserData;
-        
-        % verificar que estos parametros sean válidos
-        if isempty(n) || isempty(tol) || isempty(P), return, end
-
-        % obtención, conversión de unidades y almacenamiento de los resultados
-        [M,phi,c] = momento_curvatura(b,h,d1,d2,As1,As2,P,handles.parametros,n,tol);
-        M = 0.1*M;
-        phi = 10*phi;
-        c = 0.1*c;
-        resultadosMphi.M1 = M;
-        resultadosMphi.phi = phi;
-        resultadosMphi.c1 = c;        
-        
-        legend off
-        plot(phi,M,'linewidth',1)
-        
-        % mostrar punto donde ec=0.003
-        [~,ind_eu] = min(abs(phi.*c-0.003)); % 
-        plot(phi(ind_eu),M(ind_eu),'o','markerfacecolor','r','markeredgecolor','b')
-        
-        % si la sección no está armada simétricamente, incluir la rama con phi<0
-        if ~handles.armadurasSimetricas
-            [M2,phi2,c2] = momento_curvatura(b,h,d1,d2,As1,As2,P,handles.parametros,n,tol,-1);
-            
-            M2 = 0.1*M2;
-            phi2 = 10*phi2;
-            c2 = 0.1*c2;
-            resultadosMphi.M2 = M2;
-            resultadosMphi.phi2 = phi2;
-            resultadosMphi.c2 = c2;
-
-            plot(phi2,M2,'linewidth',1,'color',[0 0.4470 0.7410]);            
-            
-            [~,ind_eu] = min(abs(-phi2.*c2-0.003)); % 
-            plot(phi2(ind_eu),M2(ind_eu),'o','markerfacecolor','r','markeredgecolor','b')
-            
-            plot(xlim,[0 0],[0 0],ylim,'color',[0.5 0.5 0.5])
-        end
-            
-        xlabel('\phi [1/cm]')
-        ylabel('M [tonf\timesm]')
-        
-        handles.resultadosMphi = resultadosMphi;
-    end
-else
-    handles.datosValidos = false;
-end
-
-handles.figure1.Pointer = 'arrow';
-guidata(handles.figure1,handles)
-
 
 function input_carga_Callback(hObject, ~, handles)
 % incialización de outputs dependientes de la carga axial especificada
